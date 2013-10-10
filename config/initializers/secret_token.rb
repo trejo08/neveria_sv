2ad8593e828811a9451f4effaad7390170dc80c0
Juan Trejo <trejocola@gmail.com>
2013-10-09 22:31:01 -0600

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoNeveria::Application.config.secret_token = '41209d9491d4ac8f79f91fb071894982fea11f606cb05da2a91c11cc772fe34226bba01cf8a421529464d9c6dfcc5d274e473e2a1aa7167886e68447d228c4a0'
